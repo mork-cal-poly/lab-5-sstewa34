de723bfad77c4c7bd4fbc808c580b6d37859cfca
Bucky Stewart <shanbostewart@gmail.com>
2024-03-08 18:13:18 -0800

    Code added

--- src/sketchOrganic.js
+++ src/sketchOrganic.js
@@ -7,5 +7,80 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  drawBackground(100,100)
+  
+  let i = 0;
+  while (i<40) {
+    drawRain(random(5,400),random(90,250),random(0.2,1))
+    i++;
+  }
+  
+}
+
+function drawRain(x,y,s) {  //raindrop
+  push()
+  translate(x,y)
+  scale(s)
+  
+  fill(190,230,255)
+  noStroke();
+  ellipse(-100,100,50)
+  triangle(-120,85,-100,50,-80,85)
+ 
+  pop()
+}
+
+function drawBackground(x,y) {
+  push()
+  translate(x,y);
+  // background
+  background(20,130,200);
+  //clouds
+  drawCloud2(x,y,0.5)
+  drawCloud1(x,y,0.5)
+  noStroke();
+  fill(50,125,0)
+  rect(-100,250,400,50)
+  pop()
+}
+
+function drawCloud1(x,y,s){  //left cloud
+  push()
+  translate(x,y)
+  scale(s)
+  
+  fill(255)
+  beginShape();
+    curveVertex(-400,-200)
+    curveVertex(-325,-180)
+    curveVertex(-250,-200)
+    curveVertex(-130,-220)
+    curveVertex(-80,-275)
+    curveVertex(30,-330)
+    curveVertex(30,-400)
+    vertex(-400,-400)
+  endShape(CLOSE);
+  
+  pop()
+}
+
+function drawCloud2(x,y,s){  //right cloud
+  push()
+  translate(x,y)
+  scale(s)
+  
+  fill(255)
+  beginShape();
+    curveVertex(-30,-300)
+    curveVertex(50,-275)
+    curveVertex(100,-290)
+    curveVertex(160,-270)
+    curveVertex(220,-280)
+    curveVertex(300,-250)
+    curveVertex(400,-260)
+    vertex(400,-400)
+    vertex(-30,-400)
+  endShape(CLOSE);
+ 
+  pop()
 }
--- src/sketchStructured.js
+++ src/sketchStructured.js
@@ -1,3 +1,4 @@
+let xCoord = 0;
 function setup() {
   // For ordering nodes in the DOM
   let myCanvas = createCanvas(400, 400);
@@ -7,5 +8,54 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  drawBackground();
+  
+  let i = 0;
+  for (i = 0; i < 10; i++) {
+  rectWidth = random(50, 100);
+  rectHeight = random(50, 300);
+    
+    drawBuilding(xCoord, height - rectHeight, rectWidth, rectHeight);
+    
+    xCoord += rectWidth;
+  }
+}
+
+  function drawBuilding(x,y,w,h) {
+  push();
+  fill(125)
+  rect(x,y,w,h)
+  
+  let i = 0;
+  for (i = 0; i<20; i++) {
+    noStroke();
+    fill (255,255,50)
+    rect(x + 10,y + 10,15)
+    y = y + 30
+  }
+  pop();
+}
+
+function drawBackground (){
+  let r = 25
+  let g = 46
+  let b = 97
+  for (let i = 0; i <= height; i = i + 1) {
+    stroke(r, g, b);
+    line(0, i, 400, i);
+    r = r + 93 / height;
+    g = g - 42 / height;
+    b = b - 23 / height;
+  }
+  noStroke();
+  fill(220)
+  ellipse(330,70,100)
+  fill(170)
+  ellipse(330,72,80)
+  fill(220)
+  ellipse(335,65,80)
+  fill(170)
+  ellipse(350,70, 30)
+  ellipse(340,40, 20)
+  ellipse(315,60, 20)
 }
